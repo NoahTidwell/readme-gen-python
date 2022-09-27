163112c006fd66272fb5058af2132d3f0346e22f
Noah <ntidwell34@yahoo.com>
2022-09-27 00:04:44 -0500

    update README generator

--- readMeScript.py
+++ readMeScript.py
@@ -1,11 +1,16 @@
 import os
 
-save_path = '/Users/noahtidwell/Desktop/IDX/python/readme_gen_python'
+save_path = '/Users/noahtidwell/Desktop/readme_gen_python'
 file_name = 'README.md'
 x = os.path.join(save_path, file_name)
 
 header = '# '
 subtitle = '## '
+space = ''
+dscript = "Decription:"
+install = 'Installation:'
+use = 'Usage:'
+contribute = 'Contributions:'
 
 title = input("Please enter the title of your Project > ")
 description = input("Please enter a detailed description of your project > ")
@@ -16,8 +21,12 @@ contributions = input("Please enter how others can contribute to this project > 
 file1 = open(x, 'w')
 
 file1.write('{}{} \n'.format(header, title))
-file1.write('\n {}{} \n'.format(subtitle, description))
-file1.write('\n {}{} \n'.format(subtitle, installation))
-file1.write('\n {}{} \n'.format(subtitle, usage))
-file1.write('\n {}{} \n'.format(subtitle, contributions))
+file1.write('\n {}{} \n'.format(subtitle, dscript))
+file1.write('{}{} \n'.format(space, description))
+file1.write('\n {}{} \n'.format(subtitle, install))
+file1.write('{}{} \n'.format(space, installation))
+file1.write('\n {}{} \n'.format(subtitle, use))
+file1.write('{}{} \n'.format(space, usage))
+file1.write('\n {}{} \n'.format(subtitle, contribute))
+file1.write('{}{} \n'.format(space, contributions))
 file1.close()
